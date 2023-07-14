3fe33d8710dc76c06f337691392083cf4ac8bef2
TelRich <okorogoodrich@gmail.com>
2023-07-14 04:11:26 +0100

    change to title name

--- nur_app.py
+++ nur_app.py
@@ -13,13 +13,13 @@ import streamlit as st
 st.set_page_config(layout='centered', page_title='National University Rank App')
 
 # App Setup
-st.markdown("<h1 style='text-align:center;'>National University Rank App</h1>", unsafe_allow_html=True)
-st.markdown("<h3 style='text-align:center;'>Research Idea</h3>", unsafe_allow_html=True)
-
-st.markdown("""<center>
-Developing a searchable database to help high school students identify colleges that match 
-their criteria in terms of tuition, graduation rate, location, and rank.
-  </center>""", unsafe_allow_html=True)
+with st.expander(':violet[PROJECT TITLE]', expanded=True):
+  st.markdown("<h1 style='text-align:center;'>National University Rank App</h1>", unsafe_allow_html=True)
+  st.markdown("<h3 style='text-align:center;'>Research Idea</h3>", unsafe_allow_html=True)
+  st.markdown("""<center>
+  Developing a searchable database to help high school students identify colleges that match 
+  their criteria in terms of tuition, graduation rate, location, and rank.
+    </center>""", unsafe_allow_html=True)
 
 st.image('https://storage.googleapis.com/kaggle-datasets-images/2700487/4646217/3150dd9c507c09c6fe0d7e38c9cef15d/dataset-cover.png?t=2022-12-03-17-10-45')
 
