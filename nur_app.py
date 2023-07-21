bca17637186a8f1e9b74ea306e4deb625cd94286
GOODRICH OKORO <okorogoodrich@gmail.com>
2023-07-21 04:26:07 +0100

    added static image to the app

--- nur_app.py
+++ nur_app.py
@@ -11,7 +11,7 @@ import streamlit as st
 import plotly.express as px
 
 # Setting th epage size and title
-st.set_page_config(layout='centered', page_title='National University Rank App')
+st.set_page_config(layout='wide', page_title='National University Rank App')
 
 # App Setup
 with st.expander(':violet[PROJECT TITLE]', expanded=True):
@@ -23,7 +23,11 @@ with st.expander(':violet[PROJECT TITLE]', expanded=True):
     </center>""", unsafe_allow_html=True)
 
 # App Image
-st.image('https://storage.googleapis.com/kaggle-datasets-images/2700487/4646217/3150dd9c507c09c6fe0d7e38c9cef15d/dataset-cover.png?t=2022-12-03-17-10-45')
+st.markdown("""
+<p align="center">
+<img width="1050" height="500" src="https://storage.googleapis.com/kaggle-datasets-images/2700487/4646217/3150dd9c507c09c6fe0d7e38c9cef15d/dataset-cover.png?t=2022-12-03-17-10-45">
+</p>
+""", unsafe_allow_html=True)
 
 # App External Links
 st.sidebar.markdown("""
@@ -38,12 +42,36 @@ def load_data():
   return df
   
 df_nur = load_data()
-fig = px.bar(df_nur[:3], x="rank", y="name", color="name", text_auto=True,height = 500, width= 700, labels={'name':''})
-fig.update_layout(xaxis={"categoryorder": "total ascending"}, title_text="Top Universities by Rank")
-st.plotly_chart(fig)  
+
+fig1 = px.bar(df_nur[:3].sort_values('rank', ascending=False), y="rank", x="name", text_auto=True,height = 400, width= 550, labels={'name':'', 'rank':''})
+fig1.update_layout(xaxis={"categoryorder": "total ascending"}, title_text="Top Universities by Rank")
+fig1.update_yaxes(showticklabels=False)
+
+top_tf = df_nur.sort_values(by='tuition_and_fees', ascending=False)[:3]
+fig2 = px.bar(top_tf, y="tuition_and_fees", x="name", text_auto=True,height=400, width=550,
+             labels={'name':'', 'tuition_and_fees': ''})
+fig2.update_layout(xaxis={"categoryorder": "total descending"}, title_text="Top Universities by Tuition and fees")
+fig2.update_yaxes(showticklabels=False)
+
+top_ins = df_nur.sort_values(by='in_state', ascending=False)[:3]
+fig3 = px.bar(top_ins, x="in_state", y="name", text_auto=True,height=400, width=550,
+             labels={'name':'', 'in_state': ''}, title="Top Universities by In-State fees")
+fig3.update_xaxes(showticklabels=False)
+
+top_ins = df_nur[df_nur['year'] != 0].sort_values(by='year')[:3]
+fig4 = px.bar(top_ins, y="year", x="name", text_auto=True,height=400, width=550,
+             labels={'name':'', 'year': ''}, title="Oldest Universities")
+fig4.update_yaxes(showticklabels=False)
 
 with st.expander('Top Ranking School'):
-  st.text('In Progres.......')
+  col1, col2 = st.columns([3,3], gap='small')
+  with col1:
+    st.plotly_chart(fig1, use_container_width=True)
+    st.plotly_chart(fig3, use_container_width=True)  
+  with col2:
+    st.plotly_chart(fig2, use_container_width=True)
+    st.plotly_chart(fig4, use_container_width=True)
+
 
 with st.expander('School Selection'):
   st.text('In Progres.......')
