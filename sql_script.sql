2f6d3f0a78daa49b4325ced0f33b95fdde06be41
kennedy87670 <nwaizugbechukwuemeka.jk@gmail.com>
2023-08-03 22:47:06 +0100

    sql

--- sql_script.sql
+++ sql_script.sql
@@ -8,6 +8,8 @@ Order by rank DESC
 LIMIT 5
   
   
+
+
   -------Top 5 schools by tuition fees for southeast state
 Select a.name, b.tuiton fee,
 From South a
@@ -17,4 +19,19 @@ Order by  tuition fee DESC
 LIMIT 5
 
 
+-- kennedy
+-- show the top 5 schools by rank for west states. 
+select w.name, r.rank_num
+from nur_app.west as w
+inner join nur_app.rank as r
+on w.rank_id = r.id
+order by r.rank_num asc limit 5
+
+
+-- show the top 5 schools by instate fees for midwest states
+select m.name, r.in_state
+from nur_app.midwest as m
+inner join nur_app.rank as r
+on m.rank_id = r.id
+order by r.in_state Desc limit 5
 
