67d41a5bfc7fa1bd2415eb2a3ff26cfd5c4a2129
GOODRICH OKORO <okorogoodrich@gmail.com>
2023-08-18 16:20:07 +0100

    adjusted for report

--- nur_app.py
+++ nur_app.py
@@ -321,11 +321,11 @@ with st.expander(':red[Power BI Map]', expanded=True):
   components.html(power, height=700, width=1800)
 
 with st.expander(':red[Power BI Report]', expanded=True):
-  power = """<iframe title="Report Section" width="1500" height="1300" 
+  power = """<iframe title="Report Section" width="1400" height="1300" 
               src="https://app.powerbi.com/view?r=eyJrIjoiYTIyZWYyY2YtMWFmZS00MTllLWJmNzItMWMwYTRkZmRmOTIwIiwidCI6ImNlMzBlNGMzLWM4NjItNGVlZC1hMzdjLWU3NmJjODNhY2ZmYSJ9" 
               frameborder="0" allowFullScreen="true"></iframe>"""
 
-  components.html(power, height=1300, width=1500)
+  components.html(power, height=1300, width=1450)
 
 with st.expander(':red[School Recommender]', expanded=True):
   st.text('In Progres.......')
